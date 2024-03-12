61d2bff002e225ac2d66aa6916f58a7a074f5039
stg05 <stecenkogeorgii2005@gmail.com>
2024-03-13 01:30:49 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/labs2sem.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/labs2sem.dir/main.cpp.obj"
+  "labs2sem.exe"
+  "labs2sem.exe.manifest"
+  "labs2sem.pdb"
+  "liblabs2sem.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/labs2sem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/labs2sem.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for labs2sem.
+# This may be replaced when dependencies are built.
