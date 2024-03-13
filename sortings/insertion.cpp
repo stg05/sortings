db55ceb5d9649a9b93f0bedbc1d88e1171498e30
stg05 <stecenkogeorgii2005@gmail.com>
2024-03-13 04:08:00 +0300

    insertion aort added

--- sortings/insertion.cpp
+++ sortings/insertion.cpp
@@ -1,3 +1,7 @@
+//
+// Created by stg05 on 13.03.2024.
+//
+
 #include <iostream>
 #include <ctime>
 #include <random>
@@ -18,6 +22,23 @@ void print_array(int *arr, size_t size) {
 }
 
 void insertion_sort(int *arr, size_t size) {
+    for(size_t sorted = 1; sorted<size; sorted++){
+        size_t ptr = sorted-1;
+        while(true){
+            if(arr[ptr]<arr[sorted]){
+                break;
+            }
+            ptr--;
+            if(ptr == -1) break;
+        }
+
+        int cur = arr[sorted];
+        for(size_t j = sorted; j>ptr+1; j--){
+            arr[j] = arr[j-1];
+        }
+        arr[ptr+1] = cur;
+    }
+
     while(size>1) {
         for (size_t i = 0; i < size - 1; i++) {
             if (arr[i] > arr[i + 1]) {
@@ -41,7 +62,7 @@ int main() {
         arr[i] = dist(eng);
     }
     print_array(arr, arr_size);
-    bubble_sort(arr, arr_size);
+    insertion_sort(arr, arr_size);
     print_array(arr, arr_size);
 
     return 0;
